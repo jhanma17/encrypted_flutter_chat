6842cb4b937f055bd9766d7bbbedde4e187d5eee
jhanma17 <55155554+jhanma17@users.noreply.github.com>
2022-05-24 00:10:48 -0500

    Revert "Update _configuration.dart"
    This reverts commit e5636e681fe7aac359ed1a2cb280186fd23975bd.

--- lib/config/_configuration.dart
+++ lib/config/_configuration.dart
@@ -1,11 +1,11 @@
 class Configuration {
-  static const apiKey = "";
-  static const authDomain = "";
+  static const apiKey = "AIzaSyABxn3RKNrCAMe7-5_jBAQZIP-3WdoxbMk";
+  static const authDomain = "fir-chat-uninorte.web.app";
   static const databaseURL =
-      "";
-  static const projectId = "";
+      "https://console.firebase.google.com/u/0/project/fir-chat-uninorte/database/fir-chat-uninorte-default-rtdb/data/~2F";
+  static const projectId = "fir-chat-uninorte";
   static const storageBucket = "";
   static const messagingSenderId = "";
-  static const appId = "";
+  static const appId = "1:372290316083:android:fe2e4eb745d153b98d8046";
   static const measurementId = "";
 }
